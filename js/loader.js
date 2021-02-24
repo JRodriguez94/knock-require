d2fea4095cf4b1255e7f5056e92f03ae74c84c39
unknown <josh_gogo@hotmail.es>
2021-02-24 15:30:17 -0600

    Se implementa la estructura base del flujo de ejecucion de los componentes custom cargados con require. aun falta dividir el viewModel de la defiicion del componente. Punto de guardado antes de continuar

--- js/loader.js
+++ js/loader.js
@@ -1,5 +1,54 @@
-console.log('Loader :D');  
 
-require(['ko', 'tets'], function(ko) {
-    console.log('Entra al require de test..(?');
-})
+// ? Define que es declarado como funcion anonima, es decir,
+// ? Será ejecutado al hacer instancia del archivo, sin embargo 
+// ? No podrá se invocado porterior mente.
+
+define(['ko'], ko => {
+    console.log('Está entrando al define(?')
+
+    // ? Se registra el componente 'test-component'
+    // ? El template se encuentra aislado dentro del directorio
+    // ? 'Modulos'. El viewModel es declarado en este mismo archivo
+    // ? La idea es que pueda ser cargado por require como el view
+    // ? Sin embargo se enceuntra aun en proceso ese paso.
+
+    ko.components.register('test-component', {
+        // template: '<h1>Hola, este es un componente dinamico basico</h1>'
+        template: { require: 'text!../modules/test/test.html' },
+        viewModel: function(params) {
+            console.log('Está entrando al viewModel estático')
+            console.log('Params(? ', params)
+
+            let self = this;
+
+            // ? Estas son las funciones que se ejecutan meduante el bindeo
+            // ? Directamente en la definición del componente (en el view)
+            self.onClick1 = () => {
+                console.log('Está entrando al onclick 1');
+
+                // ? Es la accion definida en este mismo archivo y que
+                // ? Es pasada como parametro al componente en cuestion
+                // ? Por el momento se encuentra definida en este mimo archivo
+                // ? Sin embargo se espera que sea modulada al dividir el viewModel
+                // ? De esta definición del componente.
+                params.action();
+            }
+
+            self.onClick2 = () => {
+                console.log('Está entrando al onclick 2');
+                params.action();
+            }
+
+        }
+    });
+
+    ko.applyBindings(new mainViewModel());
+
+});
+
+function mainViewModel() {
+    let self = this;
+    self.onTest = () => {
+        console.log('Está entrando al onTest desde el main loader :d');
+    }
+}
--- js/main.js
+++ js/main.js
@@ -1,15 +1,26 @@
+// ? Configuracion para la libreria de Require
+
+// ? Punto de entrada para localizar las dependencias de require
+// ? Se definen las rutas en el objeto paths de config.
+// ? Si no es definida la propuedad baseUrl, el path base
+// ? Será relativo a la rura de este archivo
 require.config({
     paths: {
-        ko: '/js/libs/knockout',
-        jquery: '/js/libs/jquery',
-        text: '/js/libs/text',
-        loader: '/js/loader',
-        test: '/modules/test'
+        ko: 'libs/knockout',
+        jquery: 'libs/jquery',
+        text: 'libs/text',
+        // loader: '/js/loader',
     },
-    baseUrl: '/'
+    // baseUrl: ''
 });
 
-
-require(['ko', 'loader'], function(ko){
+// ? Require para mandar a llamar el archivo loader
+// ? Se le pasa como parametros las dependencias de las que
+// ? hará uso. Nota: Parece que la dependencia del archivo 
+// ? 'loader.js' no es necesaria entro de pashs, pero si no
+// ? se pasa como parametro, no se puede acceder al archivo y
+// ? por lo tanto no sigue el flujo de ejecución
+require(['ko',  'loader'], (ko) => {
     console.log('Entra al primer require con ko: ', ko);
+    // console.log('Loader: ', loader)
 });
